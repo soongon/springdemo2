4b9b50c7692e1597a0185c6f8ba4f8f06fa34a42
KITRI <soongon@gmail.com>
2023-04-12 15:19:34 +0900

    initial commit

--- src/main/java/kr/re/kitri/springdemo2/repository/TodoRepository.java
+++ src/main/java/kr/re/kitri/springdemo2/repository/TodoRepository.java
@@ -1,6 +1,7 @@
 package kr.re.kitri.springdemo2.repository;
 
 import kr.re.kitri.springdemo2.model.Todo;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
@@ -8,26 +9,9 @@ import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
-public class TodoRepository {
+@Mapper
+public interface TodoRepository {
 
-    public List<Todo> selectAllTodos() {
-        // db connection and query..
-        // Todo
-        List<Todo> todos = List.of(
-                new Todo(1, "설겆이하기", false),
-                new Todo(2, "3일차 강의하기", false),
-                new Todo(3, "맛있는 저녁먹기", false)
-        );
-        return todos;
-    }
-
-    public Todo selectTodoByTodoId(int todoId) {
-        /*Connection conn = createConnection("url", "3306", "scott", "tiger");
-        PreparedStatement pstmt = conn.prepareStatement(
-                "select * from todo where id = " + todoId);
-        ResultSet rs = pstmt.executeQuery();
-        rs.....
-*/
-        return new Todo(todoId, "가짜로만듬", false);
-    }
+    List<Todo> selectAllTodos();
+    Todo selectTodoByTodoId(int todoId);
 }
