51510f20ba3dcaa7a2409c386046aaa7655d7006
KITRI <soongon@gmail.com>
2023-04-13 16:35:07 +0900

    initial commit

--- src/main/java/kr/re/kitri/springdemo2/controller/TodoController.java
+++ src/main/java/kr/re/kitri/springdemo2/controller/TodoController.java
@@ -2,6 +2,8 @@ package kr.re.kitri.springdemo2.controller;
 
 import kr.re.kitri.springdemo2.model.Todo;
 import kr.re.kitri.springdemo2.service.TodoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +13,14 @@ import java.util.List;
 @RestController
 public class TodoController {
 
+    static final Logger logger = LoggerFactory.getLogger(TodoController.class);
+
     @Autowired
     private TodoService todoService;
 
     @GetMapping("/todos")
     public List<Todo> todos() {
-
+        logger.debug("전체보기 로그 남깁니다.");
         return todoService.viewAllTodos();
     }
 
